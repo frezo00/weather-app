ebccd31df6de5a2351bdc63c70304b82f4190f2f
Frano Rezo <frezo@obi4wan.com>
2021-02-09 12:38:45 +0100

    05: Create Search pipe + enable CDK virtual scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { SvgIconsModule } from '@ngneat/svg-icon';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import iconSettings from '../assets/svg';
 import { AppComponent } from './app.component';
@@ -11,11 +13,32 @@ import { TypeaheadComponent } from './components/typeahead/typeahead.component';
 import { OutsideClickDirective } from './directives/outside-click.directive';
 import { DaterangePipe } from './pipes/daterange.pipe';
 import { FnPipe } from './pipes/fn.pipe';
+import { SearchPipe } from './pipes/search.pipe';
 import { ApiInterceptor } from './services/interceptor/api.interceptor';
 
 @NgModule({
-  declarations: [AppComponent, DropdownComponent, TypeaheadComponent, DaterangePipe, FnPipe, OutsideClickDirective],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, SvgIconsModule.forRoot({ ...iconSettings })],
+  declarations: [
+    // COMPONENTS
+    AppComponent,
+    DropdownComponent,
+    TypeaheadComponent,
+
+    // DIRECTIVES
+    OutsideClickDirective,
+
+    // PIPES
+    DaterangePipe,
+    FnPipe,
+    SearchPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ScrollingModule,
+    SvgIconsModule.forRoot({ ...iconSettings })
+  ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
