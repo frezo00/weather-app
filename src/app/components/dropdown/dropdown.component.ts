6a8476ccd1047479462ac04d74fec3aa768bf3ee
Frano Rezo <frezo@obi4wan.com>
2021-02-09 20:33:29 +0100

    06: Minor fixes

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -6,6 +6,7 @@ import { Component, EventEmitter, Input, Output, TemplateRef } from '@angular/co
   styleUrls: ['./dropdown.component.scss']
 })
 export class DropdownComponent {
+  // NOTE: Using 'any' isn't the best way, but as this component can accept different types of objects, should be fine
   @Input() items!: any[];
   @Input() itemTemplate!: TemplateRef<HTMLElement>;
   @Input() rowHeight = 30;
