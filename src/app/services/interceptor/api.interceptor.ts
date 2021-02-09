dc27f1ff8f088fe6d96f6ca611cfcaf27f2d8fa7
Frano Rezo <frezo@obi4wan.com>
2021-02-09 12:36:59 +0100

    05: Add Loading service

--- src/app/services/interceptor/api.interceptor.ts
+++ src/app/services/interceptor/api.interceptor.ts
@@ -1,13 +1,14 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry, tap } from 'rxjs/operators';
+import { catchError, delay, finalize, retry, tap } from 'rxjs/operators';
 
 import { environment } from '../../../environments/environment';
+import { LoadingService } from '../loading.service';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private _loadingService: LoadingService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     request = request.clone({
@@ -19,7 +20,10 @@ export class ApiInterceptor implements HttpInterceptor {
     });
 
     return next.handle(request).pipe(
+      tap(_ => this._loadingService.show()),
+      delay(1000),
       retry(1),
+      finalize(() => this._loadingService.hide()),
       catchError(error => {
         return throwError(error);
       })
