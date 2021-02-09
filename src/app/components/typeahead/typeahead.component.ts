0944196598c701cd16d542dcd49472dca9f7f873
Frano Rezo <frezo@obi4wan.com>
2021-02-09 12:45:58 +0100

    05: Update Typeqahead component

--- src/app/components/typeahead/typeahead.component.ts
+++ src/app/components/typeahead/typeahead.component.ts
@@ -1,14 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
+
+import { AbstractValueAccessor, makeProvider } from '../../shared';
 
 @Component({
   selector: 'zivv-typeahead',
   templateUrl: './typeahead.component.html',
-  styleUrls: ['./typeahead.component.scss']
+  styleUrls: ['./typeahead.component.scss'],
+  providers: [makeProvider<TypeaheadComponent>(TypeaheadComponent)]
 })
-export class TypeaheadComponent implements OnInit {
-  @Input() placeholderText = 'Search';
+export class TypeaheadComponent extends AbstractValueAccessor<string> {
+  @Input() items!: any[];
+  @Input() itemTemplate: TemplateRef<HTMLElement> | undefined;
+  @Input() rowHeight = 45;
+  @Input() labelKey = '';
+  @Input() isLoading = false;
+  @Input() placeholder = 'Search';
+  @Output() itemSelect = new EventEmitter<any>();
 
-  constructor() {}
+  isOpen = false;
 
-  ngOnInit(): void {}
+  toggleOpen(value: boolean): void {
+    this.isOpen = value;
+  }
+
+  onItemSelect(selectedItem: any): void {
+    this.writeValue(this.labelKey ? selectedItem[this.labelKey] : selectedItem);
+    this.toggleOpen(false);
+    this.itemSelect.emit(selectedItem);
+  }
+
+  calculateHeight(itemsLength: number): string {
+    const maxHeight = (itemsLength || 0) * this.rowHeight;
+    return maxHeight > 160 ? '160px' : `${maxHeight}px`;
+  }
 }
